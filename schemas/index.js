5ccc6657fa2815173a0a4499aa259cff382252ca
leesin1040 <leesin1040@gmail.com>
2023-11-06 15:44:59 +0900

    올라가ㅏ

--- schemas/index.js
+++ schemas/index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connect = () => {
-  mongoose.connect(process.env.DATABASE).catch((err) => console.log(err));
+	mongoose
+		.connect(
+			`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.iukvvuy.mongodb.net/nodejsdb?retryWrites=true&w=majority`,
+		)
+		.catch((err) => console.log(err));
 };
 
 mongoose.connection.on('error', (err) => {
-  console.error('몽고디비 연결 에러', err);
+	console.error('몽고디비 연결 에러', err);
 });
 
 module.exports = connect;
