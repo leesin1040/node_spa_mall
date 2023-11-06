999d8381ff830fe5001d1e55ecca9ec92c98beac
leesin1040 <leesin1040@gmail.com>
2023-11-06 13:49:26 +0900

    Finaly

--- schemas/index.js
+++ schemas/index.js
@@ -1,11 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connect = () => {
-  mongoose
-    .connect(
-      'mongodb+srv://leesin1040:HyPRp73QLDxevm3R@cluster0.iukvvuy.mongodb.net/nodejsdb?retryWrites=true&w=majority'
-    )
-    .catch((err) => console.log(err));
+  mongoose.connect(process.env.DATABASE).catch((err) => console.log(err));
 };
 
 mongoose.connection.on('error', (err) => {
